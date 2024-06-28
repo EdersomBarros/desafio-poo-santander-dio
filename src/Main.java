aaec4abf3c4a20cc488ed61ee1b5494b24e31eff
ederson barros <edersombarros@hotmail.com>
2024-06-28 15:38:42 -0400

    include mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTutulo("Curso java");
+        curso1.setDescricao("Descrição do curso...");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTutulo("Curso PHP");
+        curso2.setDescricao("Descrição do curso...");
+        curso2.setCargaHoraria(90);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(mentoria);
+
     }
 }
